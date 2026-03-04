e92bcfcd8a86c355bf8629fce6e81cc95d48be62
Owaish-Gulzar <Yashkhan9885@gmail.com>
2026-03-04 23:19:15 +0530

    OOPS banner app using string join

---
+++ UC3.java
@@ -0,0 +1,17 @@
+package OOPSBANNERAPP;
+
+public class UC3{
+    public static void main(String[] args)
+    {
+        String pattern = String.join("\n",
+            "  ***     ***    ******    *****  ",
+            " **  **   **  **  **   **  **   ** ",
+            " **  **   **  **  **   **  **      ",
+            " **  **   **  **  ******    *****  ",
+            " **  **   **  **  **            ** ",
+            " **  **   **  **  **       **    **",
+            "  ***     ***    **        *****   "
+        );
+        System.out.println(pattern);
+    }
+}
