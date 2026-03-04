256434658e0fdd227500c9e10f7c21c6077d6637
Owaish-Gulzar <Yashkhan9885@gmail.com>
2026-03-04 23:26:34 +0530

    OOPS banner using string array and loops added

---
+++ UC4.java
@@ -0,0 +1,33 @@
+package OOPSBANNERAPP;
+
+public class UC4{
+    public static void main(String[] args)
+    {
+        String[] line = new String[7];
+
+        line[0] = String.join(" ",
+                "  ***  ", "   ***   ", " ****** ", "  *****  ");
+
+        line[1] = String.join(" ",
+                " **  ** ", "  **  ** ", " **   ** ", " **   ** ");
+
+        line[2] = String.join(" ",
+                " **  ** ", "  **  ** ", " **   ** ", " **      ");
+
+        line[3] = String.join(" ",
+                " **  ** ", "  **  ** ", " ******  ", "  *****  ");
+
+        line[4] = String.join(" ",
+                " **  ** ", "  **  ** ", " **       ", "      ** ");
+
+        line[5] = String.join(" ",
+                " **  ** ", "  **  ** ", " **       ", " **    **");
+
+        line[6] = String.join(" ",
+                "  ***   ", "   ***   ", " **       ", "  *****  ");
+        for(String lines:line)
+        {
+            System.out.println(lines);
+        }
+    }
+}
